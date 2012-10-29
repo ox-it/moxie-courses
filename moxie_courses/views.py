a63fd924a0660f5afa5245187e198d9250533d9d
David King <dave@davbo.org>
2012-10-29 16:59:12 +0000

    Moved logic into service

--- moxie_courses/views.py
+++ moxie_courses/views.py
@@ -1,15 +1,12 @@
 from moxie.core.views import ServiceView
-from moxie.auth.oauth import OAuth1Service
+from .services import CoursesService
 
 
 class ListCourses(ServiceView):
     methods = ['GET', 'OPTIONS']
-    default_allow_headers = 'geo-position'
 
     def handle_request(self):
-        oauth = OAuth1Service.from_context()
-        if oauth.authenticated:
-            # TODO: Get list of courses from the XCRI import
-            return {'authenticated': True}
-        else:
-            return {'redirect': oauth.authorization_url}
+        courses = CoursesService.from_context()
+
+        response = {'courses': courses.list_courses()}
+        return response
