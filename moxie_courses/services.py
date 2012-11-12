3c98dc681cade8fcfd45863029ff204361674f76
David King <dave@davbo.org>
2012-11-12 14:27:40 +0000

    CoursesService shouldn't depend on OAuth

--- moxie_courses/services.py
+++ moxie_courses/services.py
@@ -1,13 +1,11 @@
 from moxie.core.service import Service
-from moxie.oauth.services import OAuth1Service
 
 
 class CoursesService(Service):
     default_search = '*'
 
-    def list_courses(self):
-        oauth = OAuth1Service.from_context()
+    def list_courses(self, authorized=False):
         courses = [{'name': 'SCRUM Master training 4.5'}]
-        if oauth.authorized:
+        if authorized:
             courses.append({'name': 'Oxford Special Forces'})
         return courses
--- moxie_courses/views.py
+++ moxie_courses/views.py
@@ -1,4 +1,5 @@
 from moxie.core.views import ServiceView
+from moxie.oauth.services import OAuth1Service
 from .services import CoursesService
 
 
@@ -6,7 +7,10 @@ class ListCourses(ServiceView):
     methods = ['GET', 'OPTIONS']
 
     def handle_request(self):
+        # Get our services
         courses = CoursesService.from_context()
+        oauth = OAuth1Service.from_context()
 
-        response = {'courses': courses.list_courses()}
+        course_list = courses.list_courses(authorized=oauth.authorized)
+        response = {'courses': course_list}
         return response
