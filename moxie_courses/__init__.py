1c64661f57ae2affc99d063c6c890ba5f5030145
David King <dave@davbo.org>
2012-11-14 16:53:06 +0000

    Register view

--- moxie_courses/__init__.py
+++ moxie_courses/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 from moxie import oauth
-from .views import ListCourses
+from .views import ListCourses, Bookings
 
 
 def create_blueprint(blueprint_name):
@@ -10,6 +10,8 @@ def create_blueprint(blueprint_name):
     # URL Rules
     courses_blueprint.add_url_rule('/courses',
             view_func=ListCourses.as_view('list'))
+    courses_blueprint.add_url_rule('/bookings',
+            view_func=Bookings.as_view('bookings'))
     oauth.attach_oauth(courses_blueprint)
 
     return courses_blueprint
