bcf2af8679ec8c8f0bac3eb685ee68c9fd744612
Martin Filliau <martin@filliau.com>
2012-11-14 12:33:18 +0000

    Added support of start/end/applyFrom/applyUntil dates.

--- moxie_courses/importers/xcri_ox.py
+++ moxie_courses/importers/xcri_ox.py
@@ -13,22 +13,26 @@ OXCAP_NS = "http://purl.ox.ac.uk/oxcap/ns/"
 MLO_NS = "http://purl.org/net/mlo"
 DC_NS = "http://purl.org/dc/elements/1.1/"
 
-# Elements to keep in
+# Elements to keep in, represents a document
+# If the value is not None, this is the attribute that will be used
 PARSE_STRUCTURE = {
-    (XCRI_NS, "provider"): [
-        (DC_NS, "identifier"),
-        (DC_NS, "title"),
-        ],
-    (XCRI_NS, "course"): [
-        (DC_NS, "description"),
-        (DC_NS, "subject"),
-        (DC_NS, "identifier"),
-        (DC_NS, "title"),
-        ],
-    (XCRI_NS, "presentation"): [
-        (DC_NS, "identifier"),
-        (OXCAP_NS, "bookingEndpoint"),
-        ],
+    (XCRI_NS, "provider"): {
+            (DC_NS, "title"): None,
+        },
+    (XCRI_NS, "course"): {
+            (DC_NS, "description"): None,
+            (DC_NS, "subject"): None,
+            (DC_NS, "identifier"): None,
+            (DC_NS, "title"): None,
+        },
+    (XCRI_NS, "presentation"): {
+            (DC_NS, "identifier"): None,
+            (OXCAP_NS, "bookingEndpoint"): None,
+            (MLO_NS, "start"): "dtf",
+            (XCRI_NS, "end"): "dtf",
+            (XCRI_NS, "applyFrom"): "dtf",
+            (XCRI_NS, "applyUntil"): "dtf",
+            },
 }
 
 
@@ -41,22 +45,31 @@ class XcriOxHandler(sax.ContentHandler):
         self.tag = None     # current name of the key
         self.capture_data = False
 
-    def startElementNS(self, (uri, localname), qname, attrs):
+    def startElementNS(self, (uri, localname), qname, attributes):
         self.capture_data = False
         if (uri, localname) in PARSE_STRUCTURE:
             self.parse = (uri, localname)
         elif self.parse is not None:
-            elements = PARSE_STRUCTURE[self.parse]
-            if (uri, localname) in elements:
-                self.capture_data = True
+            element = PARSE_STRUCTURE[self.parse]
+            if (uri, localname) in element:
+                attr = element[(uri, localname)]
+                self.capture_data = attributes.getLength() == 0
                 self.tag = "{element}_{key}".format(
                     element=self.parse[1],
                     key=localname)
+                for name, value in attributes.items():
+                    qname = attributes.getQNameByName(name)
+                    prefix, property = self._split_qname(qname)
+                    if property == attr:
+                        # Use the value of the attribute instead of the element
+                        self.element_data[self.tag] = value
+                        self.capture_data = False
 
     def endElementNS(self, (uri, localname), qname):
-        if uri == XCRI_NS and localname == "presentation":
+        if (uri, localname) == (XCRI_NS, "presentation"):
             logger.debug(self.element_data)
             self.presentations.append(self.element_data)
+            self.parse = None
             self.element_data = defaultdict(list)
 
     def endDocument(self):
@@ -66,6 +79,21 @@ class XcriOxHandler(sax.ContentHandler):
         if self.capture_data:
             if data.strip():
                 self.element_data[self.tag].append(data.strip())
+
+    @classmethod
+    def _split_qname(self, qname):
+        """Split a QName in an attempt to find the prefix
+        :param qname: QName
+        :return tuple with prefix and localname; prefix None if no NS
+        """
+        qname_split = qname.split(':')
+        if len(qname_split) == 2:
+            prefix, local = qname_split
+        else:
+            prefix = None
+            local = qname
+        return prefix, local
+
 
 
 class XcriOxImporter(object):
@@ -86,9 +114,31 @@ class XcriOxImporter(object):
             parser.feed(buffered_data)
             buffered_data = self.xcri_file.read(self.buffer_size)
         parser.close()
-        self.indexer.index(self.handler.presentations)
+        return
+        for presentation in self.handler.presentations:
+            try:
+                presentation['provider_title'] = presentation['provider_title'][0]
+                presentation['course_title'] = presentation['course_title'][0]
+                presentation['course_identifier'] = presentation['course_identifier'][0]
+                presentation['presentation_identifier'] = presentation['presentation_identifier'][0]
+                self.indexer.index([presentation])
+            except Exception as e:
+                logger.debug(presentation)
+                logger.debug(e)
         self.indexer.commit()
 
+    @classmethod
+    def date_to_solr_format(self, date):
+        """Transforms date from '2008-01-01' to '2008-01-01T00:00:00Z'
+        :param date: date to format
+        :return date formatted as 2008-01-01T00:00:00Z
+        """
+        elements = date.split('-')
+        return "{year}-{month}-{day}T00:00:00Z".format(
+            year=elements[0],
+            month=elements[1],
+            day=elements[2]
+        )
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
--- moxie_courses/tests/test_xcri_ox.py
+++ moxie_courses/tests/test_xcri_ox.py
@@ -2,8 +2,10 @@ import unittest
 import logging
 
 from xml import sax
+from mock import Mock
+from moxie.core.search import SearchService, SearchResponse
 
-from moxie_courses.importers.xcri_ox import XcriOxHandler
+from moxie_courses.importers.xcri_ox import XcriOxHandler, XcriOxImporter
 
 
 class XcriOxImporterTestCase(unittest.TestCase):
@@ -12,16 +14,36 @@ class XcriOxImporterTestCase(unittest.TestCase):
         logging.basicConfig(level=logging.DEBUG)
         # 1 provider, 2 courses, 8 presentations (total)
         self.xcri_path = 'moxie_courses/tests/data/xcri.xml'
-        self.xcri_file = open(self.xcri_path)
-        self.xml_handler = XcriOxHandler()
+        self.mock_index = Mock(spec=SearchService)
+        #self.mock_index.search_for_ids.return_value = SearchResponse({'response': {'docs': []}}, None, [])
+
+
+    def test_handler(self):
+        xcri_file = open(self.xcri_path)
+        xml_handler = XcriOxHandler()
         parser = sax.make_parser()
-        parser.setContentHandler(self.xml_handler)
+        parser.setContentHandler(xml_handler)
         parser.setFeature(sax.handler.feature_namespaces, 1)
-        buffered_data = self.xcri_file.read(8192)
+        buffered_data = xcri_file.read(8192)
         while buffered_data:
             parser.feed(buffered_data)
-            buffered_data = self.xcri_file.read(8192)
+            buffered_data = xcri_file.read(8192)
         parser.close()
 
-    def test_counts(self):
-        self.assertEqual(len(self.xml_handler.presentations), 8)
+        self.assertEqual(len(xml_handler.presentations), 8)
+
+    def test_importer(self):
+        importer = XcriOxImporter(self.mock_index, open(self.xcri_path))
+        importer.run()
+
+    def test_handler_split_qname(self):
+        self.assertEqual(XcriOxHandler._split_qname("prefix:property"),
+            ('prefix', 'property'))
+        self.assertEqual(XcriOxHandler._split_qname("property"),
+            (None, 'property'))
+
+    def test_importer_date_to_solr_format(self):
+        self.assertEqual(XcriOxImporter.date_to_solr_format("2012-01-01"),
+            "2012-01-01T00:00:00Z")
+        self.assertEqual(XcriOxImporter.date_to_solr_format("2012-12-31"),
+            "2012-12-31T00:00:00Z")
