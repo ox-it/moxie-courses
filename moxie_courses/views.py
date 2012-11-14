af8fac473e8e0f931b17c9e9598bc5d3ff341ba5
David King <dave@davbo.org>
2012-11-14 16:52:10 +0000

    Bookings view and renamed CourseService

--- moxie_courses/views.py
+++ moxie_courses/views.py
@@ -1,6 +1,6 @@
 from moxie.core.views import ServiceView
 from moxie.oauth.services import OAuth1Service
-from .services import CoursesService
+from .services import CourseService
 
 
 class ListCourses(ServiceView):
@@ -8,9 +8,21 @@ class ListCourses(ServiceView):
 
     def handle_request(self):
         # Get our services
-        courses = CoursesService.from_context()
+        courses = CourseService.from_context()
         oauth = OAuth1Service.from_context()
 
         course_list = courses.list_courses(authorized=oauth.authorized)
+        response = {'courses': course_list}
+        return response
+
+
+class Bookings(ServiceView):
+    methods = ['GET', 'OPTIONS']
+
+    def handle_request(self):
+        courses = CourseService.from_context()
+        oauth = OAuth1Service.from_context()
+
+        course_list = courses.my_courses(signer=oauth.signer)
         response = {'courses': course_list}
         return response
