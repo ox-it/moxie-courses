b372f53b925db8f86bcb733d69e8594f8d711344
Martin Filliau <martin@filliau.com>
2012-11-15 17:16:21 +0000

    Updated tests to the last version of the XCRI format.

--- moxie_courses/tests/test_xcri_ox.py
+++ moxie_courses/tests/test_xcri_ox.py
@@ -12,7 +12,7 @@ class XcriOxImporterTestCase(unittest.TestCase):
 
     def setUp(self):
         logging.basicConfig(level=logging.DEBUG)
-        # 1 provider, 3 courses, 5 presentations (total)
+        # 1 provider, 3 courses, 4 presentations (total)
         self.xcri_path = 'moxie_courses/tests/data/xcri.xml'
         self.mock_index = Mock(spec=SearchService)
         #self.mock_index.search_for_ids.return_value = SearchResponse({'response': {'docs': []}}, None, [])
@@ -30,18 +30,28 @@ class XcriOxImporterTestCase(unittest.TestCase):
             buffered_data = xcri_file.read(8192)
         parser.close()
 
-        self.assertEqual(len(xml_handler.presentations), 5)
+        self.assertEqual(len(xml_handler.presentations), 4)
 
     def test_importer(self):
         importer = XcriOxImporter(self.mock_index, open(self.xcri_path))
         importer.run()
         presentations = importer.presentations
-        first = presentations[0]
 
+        first = presentations[0]
+        self.assertEqual(first['provider_title'], "Humanities Division")
         self.assertEqual(first['course_title'], "Monograph Publishing Workshop")
         self.assertEqual(len(first['course_subject']), 1)
-        self.assertEqual(first['presentation_start'], "2012-06-14T00:00:00Z")
+        self.assertEqual(first['presentation_start'], "2012-06-15T00:00:00Z")
         self.assertEqual(first['presentation_bookingEndpoint'], "https://weblearn.ox.ac.uk/course-signup/rest/course/5E00D50013")
+
+        last = presentations[3]
+        self.assertEqual(last['presentation_identifier'], "daisy/presentation/19303")
+        self.assertEqual(last['presentation_start'], "2012-05-24T00:00:00Z")
+        self.assertEqual(last['presentation_end'], "2012-05-24T00:00:00Z")
+        self.assertEqual(last['presentation_applyFrom'], "2012-04-19T00:00:00Z")
+        self.assertEqual(last['presentation_applyUntil'], "2012-05-14T00:00:00Z")
+        self.assertEqual(last['course_title'], "Lunchtime Briefings on the Digital Humanities")
+        self.assertEqual(last['provider_title'], "Humanities Division")
 
     def test_handler_split_qname(self):
         self.assertEqual(XcriOxHandler._split_qname("prefix:property"),
@@ -56,7 +66,8 @@ class XcriOxImporterTestCase(unittest.TestCase):
             "2012-12-31T00:00:00Z")
 
     def test_importer_get_identifier(self):
-        self.assertEqual(XcriOxImporter._get_identifier(['ABCD', 'https://m.ox.ac.uk/courses/1234']),
-            "1234")
-        self.assertEqual(XcriOxImporter._get_identifier(['ABCD', '12']),
-            None)
+        self.assertEqual(XcriOxImporter._get_identifier(['ABCD', 'https://course.data.ox.ac.uk/id/itlp/presentation/TDAE-106830']),
+            "itlp-presentation-TDAE-106830")
+        self.assertEqual(XcriOxImporter._get_identifier(['ABCD', '12',
+                                                         'https://course.data.ox.ac.uk/id/daisy/presentation/14941']),
+            'daisy-presentation-14941')
