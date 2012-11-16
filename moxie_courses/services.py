075099262101da13c37ac952a97daa7d4d19e7f1
Martin Filliau <martin@filliau.com>
2012-11-16 15:09:03 +0000

    Draft queries to search courses and display courses subjects.

--- moxie_courses/services.py
+++ moxie_courses/services.py
@@ -1,5 +1,7 @@
-from moxie.core.service import ProviderService
 from itertools import chain
+
+from moxie.core.service import ProviderService
+from moxie.core.search import searcher
 
 
 class CourseService(ProviderService):
@@ -13,3 +15,28 @@ class CourseService(ProviderService):
 
     def my_courses(self, signer):
         return chain([p.user_courses(signer=signer) for p in self.providers])
+
+    def search_courses(self, search):
+        """Search for courses
+        :param search: search query (FTS)
+        :return list of courses (titles and identifiers)
+        """
+        # TODO search parameters for Solr. Should be made generic. Discuss.
+        q = {'q': search,
+             'group': 'true',
+             'group.field': 'course_identifier',
+             'group.count': '1',
+             'fl': 'course_title,course_identifier',
+             }
+        results = searcher.search(q)
+        print results['grouped']['course_identifier']['groups']
+
+    def list_courses_subject(self):
+        """List all subjects from courses
+        :return list of subjects
+        """
+        q = { 'facet': 'true',
+              'facet.field': 'course_subject'
+              }
+        results = searcher.search(q)
+        print results['facet_counts']['facet_fields']['course_subject']
