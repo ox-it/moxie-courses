caab312af2f2511705b0c40da89d9b493f3090aa
Martin Filliau <martin@filliau.com>
2012-11-19 18:13:44 +0000

    Added HAL links in views (search and details).

--- moxie_courses/views.py
+++ moxie_courses/views.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, url_for
 
 from moxie.core.views import ServiceView
 from moxie.oauth.services import OAuth1Service
@@ -38,6 +38,8 @@ class SearchCourses(ServiceView):
         query = request.args.get('q', '')
         courses = CourseService.from_context()
         results = courses.search_courses(query)
+        for result in results:
+            result['_links'] = { 'self': url_for('.course', id=result['id'])}
         return {'results': results}
 
 
@@ -48,8 +50,10 @@ class CourseDetails(ServiceView):
 
     def handle_request(self, id):
         service = CourseService.from_context()
-        # path = url_for('places.poidetail', ident=doc['id'])
-        return service.list_presentations_for_course(id)._to_json()
+        course = service.list_presentations_for_course(id)._to_json()
+        course['_links'] = { 'self': url_for('.course', id=id) }
+        # TODO add link to book a presentation
+        return course
 
 
 class Bookings(ServiceView):
