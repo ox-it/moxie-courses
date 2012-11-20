2e651d34ff2992e4761373f7fb89e71c0a8ed804
David King <dave@davbo.org>
2012-11-20 18:00:53 +0000

    Fixed a bug in listing user courses with chain

--- moxie_courses/services.py
+++ moxie_courses/services.py
@@ -22,7 +22,7 @@ class CourseService(ProviderService):
         :param signer: OAuth signer token of the user
         :return list of Course objects
         """
-        return chain([p.user_courses(signer=signer) for p in self.providers])
+        return chain(*[p.user_courses(signer=signer) for p in self.providers])
 
     def search_courses(self, search):
         """Search for courses
