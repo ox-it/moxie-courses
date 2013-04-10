e6c948215fdb3d7797489ee2810e4f8d5b17c01f
Martin Filliau <martin@filliau.com>
2013-04-10 14:52:01 +0100

    Added root route to give a list of methods available.

--- moxie_courses/__init__.py
+++ moxie_courses/__init__.py
@@ -1,6 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask.helpers import make_response
 
 from moxie import oauth
+from moxie.core.representations import HALRepresentation
 from .views import (Bookings, ListAllSubjects, SearchCourses, CourseDetails,
         PresentationBooking)
 
@@ -8,7 +10,8 @@ from .views import (Bookings, ListAllSubjects, SearchCourses, CourseDetails,
 def create_blueprint(blueprint_name):
     courses_blueprint = Blueprint(blueprint_name, __name__)
 
-    # URL Rules
+    courses_blueprint.add_url_rule('/', view_func=get_routes)
+
     courses_blueprint.add_url_rule('/bookings',
             view_func=Bookings.as_view('bookings'))
     courses_blueprint.add_url_rule('/subjects',
@@ -22,3 +25,19 @@ def create_blueprint(blueprint_name):
     oauth.attach_oauth(courses_blueprint)
 
     return courses_blueprint
+
+
+def get_routes():
+    path = request.path
+    representation = HALRepresentation({})
+    representation.add_curie('hl', 'http://moxie.readthedocs.org/en/latest/http_api/courses.html#{rel}')
+    representation.add_link('self', '{bp}'.format(bp=path))
+    representation.add_link('hl:bookings', '{bp}bookings'.format(bp=path), title="Bookings")
+    representation.add_link('hl:subjects', '{bp}subjects'.format(bp=path), title="Subjects")
+    representation.add_link('hl:search', '{bp}search?q={{q}}'.format(bp=path),
+                            templated=True, title='Search')
+    representation.add_link('hl:course', '{bp}course/{{id}}'.format(bp=path),
+                            templated=True, title='Course details')
+    response = make_response(representation.as_json(), 200)
+    response.headers['Content-Type'] = "application/json"
+    return response
